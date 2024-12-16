f2fa4217d6d501193eae8ce33131c796a2e4f8b9
JuanLara18 <jlara@unal.edu.co>
2024-12-16 18:19:45 -0500

    for initial build

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort, send_from_directory
+from flask import Flask, render_template
 from flask_frozen import Freezer
 from datetime import datetime
 import os
@@ -15,9 +15,6 @@ def create_app(test_config=None):
         CONTENT_DIR=os.path.abspath(os.path.join(app.root_path, '..', 'content')),
     )
 
-    if test_config:
-        app.config.update(test_config)
-
     freezer = Freezer(app)
     note_generator = NoteGenerator(app.config['CONTENT_DIR'])
 
@@ -25,47 +22,18 @@ def create_app(test_config=None):
     def utility_processor():
         return {
             'current_year': datetime.now().year,
-            'categories': note_generator.get_categories(),
             'site_name': 'Study Notes'
         }
 
     @app.route('/')
     def index():
-        categories = note_generator.get_categories()
-        recent_notes = note_generator.get_all_notes()[:5]
-        return render_template('index.html',
-                            categories=categories,
-                            recent_notes=recent_notes)
-
-    @app.route('/<path:note_path>')
-    def note(note_path):
-        note_data = note_generator.get_note_by_url(note_path)
-        if note_data is None:
-            abort(404)
-        return render_template('note.html', note=note_data)
-
-    @app.route('/category/<category>')
-    def category(category):
-        categories = note_generator.get_categories()
-        if category not in categories:
-            abort(404)
-        return render_template('index.html',
-                            categories={category: categories[category]},
-                            category_name=category)
+        notes = note_generator.get_all_notes()
+        return render_template('index.html', notes=notes)
 
     @freezer.register_generator
-    def note_url_generator():
-        for note in note_generator.get_all_notes():
-            yield 'note', {'note_path': note['url'].lstrip('/')}
-
-    @freezer.register_generator
-    def category_url_generator():
-        for category in note_generator.get_categories():
-            yield 'category', {'category': category}
+    def index_url_generator():
+        yield 'index', {}
 
     return app, freezer
 
 app, freezer = create_app()
-
-if __name__ == '__main__':
-    app.run()
